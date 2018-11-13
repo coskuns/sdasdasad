f897572f2e8a4cfcfa5bddf16291c264441ceb2b
coskuns <44931702+coskuns@users.noreply.github.com>
2018-11-13 19:34:33 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 
 exports.run = function(client, message) {
-message.channel.bulkDelete(20);
-message.channel.send("20 mesaj sildim").then(msg => {
+message.channel.bulkDelete(100);
+message.channel.send("100 mesaj sildim").then(msg => {
 	msg.delete(5000)
 })
 
