1268e0b793f5dc83b9e1781468d41b646b0e1e27
coskuns <44931702+coskuns@users.noreply.github.com>
2018-11-13 19:36:15 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 
 
-exports.run = function(client, message) {
-message.channel.bulkDelete(100);
-message.channel.send("100 mesaj sildim").then(msg => {
-	msg.delete(5000)
-})
-
+exports.help = {
+  name: 'temizle', 
+  description: 'Belirtilen miktarda mesaj siler',
+  usage: 'temizle <miktar>'
+};
 };
 
 exports.conf = {
