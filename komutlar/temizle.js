7ee55cd93430b2b5ad5a7bac3a6aeb7c90d69319
coskuns <44931702+coskuns@users.noreply.github.com>
2018-11-13 19:38:19 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,13 +1,6 @@
 const Discord = require('discord.js');
 
 
-exports.help = {
-  name: 'temizle', 
-  description: 'Belirtilen miktarda mesaj siler',
-  usage: 'temizle <miktar>'
-};
-};
-
 exports.conf = {
   enabled: true, 
   guildOnly: false, 
